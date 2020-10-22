817f7b80453e9892a5d761a759a36f8733b48fee
troyAmlee <troyamlee19@gmail.com>
2020-10-22 02:14:09 -0500

    [parity.py] implemented a cache solution... testing

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,9 +1,27 @@
 from test_framework import generic_test
 
-
+# How would you compute the parity of a very large number of 64 bit words?
+# Odd number of ones '1101 == 1'
+# Even number of ones '100001000 == 0'
+d = {}
 def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    summ = 0
+    conv = bin(x)
+    stringify = str(conv)
+    listify = list(stringify)
+    if(x not in d):
+        for i in listify:
+            if(i == "1"):
+                summ += 1
+        if(summ % 2 == 0):
+            d[x] = 0
+            return 0
+        elif(summ % 2 == 1):
+            d[x] = 1
+            return 1
+    else:
+        return d[x]
+
 
 
 if __name__ == '__main__':
