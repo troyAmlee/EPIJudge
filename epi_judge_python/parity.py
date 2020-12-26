d3beae893a728b7d1f25e340cf178d9cca896c71
troyAmlee <troyamlee19@gmail.com>
2020-12-25 20:28:58 -0600

    [practice] added a bitwise operation solution

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,9 +1,25 @@
 from test_framework import generic_test
 
-
+cache = {}
 def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    count = 0
+
+    while x:
+        if(x in cache):
+            return cache[x]
+        if(x & 1):
+            count += 1
+            x >>= 1
+        else:
+            x >>= 1
+    count = (count & 1)
+    cache[x] = count
+    return count
+    # 10011001
+    # check lowermost bit if it is a one
+    # count it
+    # otherwise: shift to the right by 1
+    
 
 
 if __name__ == '__main__':
