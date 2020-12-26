a8cd39fbf86e0f9b06445bb7da68d0e6f5fa38db
troyAmlee <troyamlee19@gmail.com>
2020-12-26 17:45:30 -0600

    [practice] completed swap bits

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,6 +1,5 @@
 from test_framework import generic_test
 
-<<<<<<< HEAD
 cache = {}
 def parity(x: int) -> int:
     count = 0
@@ -18,35 +17,10 @@ def parity(x: int) -> int:
     return count
     # 10011001
     # check lowermost bit if it is a one
-    # count it
+        # count it
+        # shift to the right by 1
     # otherwise: shift to the right by 1
     
-=======
-# How would you compute the parity of a very large number of 64 bit words?
-# Odd number of ones '1101 == 1'
-# Even number of ones '100001000 == 0'
-d = {}
-def parity(x: int) -> int:
-    summ = 0
-    conv = bin(x)
-    stringify = str(conv)
-    listify = list(stringify)
-    if(x not in d):
-        
-        for i in listify:
-            if(i == "1"):
-                summ += 1
-        if(summ % 2 == 0):
-            d[x] = 0
-            return 0
-        elif(summ % 2 == 1):
-            d[x] = 1
-            return 1
-    else:
-        print("---CACHE ACCESSED---")
-        return d[x]
-
->>>>>>> dc2e73235480a77fc25edf52684487bfbd838cd0
 
 
 if __name__ == '__main__':
--- epi_judge_python/swap_bits.py
+++ epi_judge_python/swap_bits.py
@@ -2,8 +2,27 @@ from test_framework import generic_test
 
 
 def swap_bits(x, i, j):
-    # TODO - you fill in here.
-    return 0
+    if((x >> i) & 1 == (x >> j) & 1):
+        return x
+
+    print(f"i extraction {(x >> i) & 1}")
+    print(f"j extraction {(x >> j) & 1}")
+
+    if(i == 0):
+        mask1 = 1
+    else:
+        mask1 = 1 << i
+    if(j == 0):
+        mask2 = 1
+    else:
+        mask2 = 1 << j
+
+    x ^= mask1
+
+    x ^= mask2
+
+    return x
+    
 
 
 if __name__ == '__main__':
