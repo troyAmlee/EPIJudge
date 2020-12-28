dd5c19f8e6dc0451e5e59b6a6f6148fae875ccf0
troyAmlee <troyamlee19@gmail.com>
2020-12-27 18:58:05 -0600

    [practice] solved reverse 64 bit int

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -2,8 +2,12 @@ from test_framework import generic_test
 
 
 def reverse_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    # pre-compute a lookup ta5ble with all reversal combinations of 16-bit integers
+    mask = 2**64
+    rev = '0b' + bin(x)[:1:-1]
+    zeros = len(bin(mask)) - len(rev) - 1
+    rev += ('0' * zeros)
+    return int(rev, 2)
 
 
 if __name__ == '__main__':
